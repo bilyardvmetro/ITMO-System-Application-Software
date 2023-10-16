f9db442fb3ca8e6dd480d79b8b97dd8cb03eaa45
Kirill Polenov <polenovkirill@gmail.com>
2023-10-16 10:34:09 +0300

    :heavy_plus_sign: add scripts for first OPD Lab

---
+++ OPD (основы проффесиональной деятельности)/Lab1/Lab0Step2.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+cd lab0
+chmod 004 chimchar4.txt
+chmod 571 growlithe1
+cd growlithe1
+chmod 330 alakazam
+chmod 004 excadrill.txt
+chmod g+w-r ursaring.txt
+chmod u-w butterfree
+chmod g-rx+w butterfree
+chmod o-x butterfree
+chmod 404 venusaur.txt
+cd ..
+chmod 004 kadabra3.txt
+chmod o-r+w nidorina0
+cd nidorina0
+chmod g-r+w nidorino.txt
+chmod o-r nidorino.txt
+chmod 330 cubchoo
+chmod 444 lileep.txt
+chmod u-w chimchar
+chmod g+w chimchar
+chmod o+w chimchar
+cd ..
+chmod 750 scizor4
+cd scizor4
+chmod 440 braviary.txt
+chmod 404 voltorb.txt
+chmod u-r zoroark
+chmod g+w zoroark
+cd ..
+chmod 044 vigoroth1.txt
+cd ..
