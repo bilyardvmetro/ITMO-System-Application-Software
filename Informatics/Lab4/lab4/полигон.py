f1e1318a91bc32f915da17158dbc77dd737d066e
Kirill Polenov <146551682+bilyardvmetro@users.noreply.github.com>
2023-11-07 17:11:42 +0300

    :fire: delete .py test file inf lab4

--- Informatics/Lab4/lab4/полигон.py
+++
@@ -1,8 +0,0 @@
-import re
-
-s = '<day id="1" name="Понедельник" hui="fdfdfdf">\n<class id="1">'
-lines = re.split('\n', s)
-print(lines, '\n')
-
-match = re.search(r'<([^/]\w+(\s+\w+="\w+")+)>', lines[0])
-print(match[1].split())
