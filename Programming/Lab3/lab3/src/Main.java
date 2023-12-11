e8ef54ee433bdb7bb6e184fc98301f292163f5e7
Kirill Polenov <polenovkirill@gmail.com>
2023-12-12 01:36:28 +0300

    :construction: updates

--- Programming/Lab3/lab3/src/Main.java
+++ Programming/Lab3/lab3/src/Main.java
@@ -4,6 +4,7 @@ import Classes.Entities.EarthShell;
 import Classes.Entities.Planet;
 import Classes.Entities.SpaceConference;
 import Classes.Groups.EarthInhabitants;
+import Classes.Groups.GroupOfScientists;
 import Classes.Groups.LunarAstronomers;
 import Classes.Persons.Alpha;
 import Classes.Persons.Memega;
@@ -45,11 +46,12 @@ public class Main {
                 + memega.sayType() + " -" + memega.call() + ".");
 
         // Четвёртое предложение
+        GroupOfScientists alphaAndMemega = new GroupOfScientists(alpha, memega);
         NightSky nightSky = new NightSky();
         StarsAndConstellations starsAndConstellations = new StarsAndConstellations();
         Sun sun = new Sun();
         Moon moon = new Moon();
-        System.out.println(Person.askProperly(alpha, memega, neznaika) + " о том, " + nightSky.hasView()
+        System.out.println(alphaAndMemega.askSomebody(neznaika) + " о том, " + nightSky.hasView()
                 + ", если на него глядеть с " + earth.getName() + ", " + starsAndConstellations.hasView() + ", а также "
                 + planet.getName() + ", " + sun.hasView() + " и " + moon.hasView() + ".");
     }
